f02b2464644fe97d846269f4bd8d8d28bd5dc6f0
Horsars <efebeosas@gmail.com>
2022-04-13 16:40:43 -0700

    correced functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,11 @@
-fndef FUNC_H
-#define FUNC_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
